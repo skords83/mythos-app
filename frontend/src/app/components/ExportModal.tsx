1e3caa488322d2c569ba7e08c93f5900bd228993
skords83 <skords@ik.me>
2026-03-09 21:39:00 +0100

    export fix

--- frontend/src/app/components/ExportModal.tsx
+++ frontend/src/app/components/ExportModal.tsx
@@ -120,25 +120,44 @@ if (format === 'pdf') {
     <rootfile full-path="OEBPS/content.opf" media-type="application/oebps-package+xml"/>
   </rootfiles>
 </container>`)
-      
+
+      const bookId = `book-${Date.now()}`
+
       const contentOpf = `<?xml version="1.0" encoding="UTF-8"?>
 <package version="2.0" xmlns="http://www.idpf.org/2007/opf" unique-identifier="bookid">
   <metadata xmlns:dc="http://purl.org/dc/elements/1.1/">
     <dc:title>${title}</dc:title>
     <dc:language>de</dc:language>
-    <dc:identifier id="bookid">${Date.now()}</dc:identifier>
+    <dc:identifier id="bookid">${bookId}</dc:identifier>
   </metadata>
   <manifest>
-    <item id="nav" href="toc.ncx" media-type="application/x-dtbncx+xml"/>
-    <item id="content" href="OEBPS/content.xhtml" media-type="application/xhtml+xml"/>
-    ${images.map(img => `<item id="${img.id}" href="OEBPS/images/${img.id}.${img.type}" media-type="image/${img.type}"/>`).join('\n    ')}
+    <item id="ncx" href="toc.ncx" media-type="application/x-dtbncx+xml"/>
+    <item id="content" href="content.xhtml" media-type="application/xhtml+xml"/>
+    ${images.map(img => `<item id="${img.id}" href="images/${img.id}.${img.type}" media-type="image/${img.type}"/>`).join('\n    ')}
   </manifest>
-  <spine toc="nav">
+  <spine toc="ncx">
     <itemref idref="content"/>
   </spine>
 </package>`
-      
-const contentXhtml = `<?xml version="1.0" encoding="UTF-8"?>
+
+      const tocNcx = `<?xml version="1.0" encoding="UTF-8"?>
+<ncx xmlns="http://www.daisy.org/z3986/2005/ncx/" version="2005-1">
+  <head>
+    <meta name="dtb:uid" content="${bookId}"/>
+    <meta name="dtb:depth" content="1"/>
+    <meta name="dtb:totalPageCount" content="0"/>
+    <meta name="dtb:maxPageNumber" content="0"/>
+  </head>
+  <docTitle><text>${title}</text></docTitle>
+  <navMap>
+    <navPoint id="navpoint-1" playOrder="1">
+      <navLabel><text>${title}</text></navLabel>
+      <content src="content.xhtml"/>
+    </navPoint>
+  </navMap>
+</ncx>`
+
+      const contentXhtml = `<?xml version="1.0" encoding="UTF-8"?>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head><title>${title}</title></head>
 <body>
@@ -158,6 +177,7 @@ ${processedContent}
       })
 
       zip.file('OEBPS/content.opf', contentOpf)
+      zip.file('OEBPS/toc.ncx', tocNcx)
       zip.file('OEBPS/content.xhtml', contentXhtml)
       
       const blob = await zip.generateAsync({ type: 'blob' })
